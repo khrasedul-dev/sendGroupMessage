59d002670d902af0bcef3f9229450f363d5336d8
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-01-09 01:03:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 //Paste API_ID without quotation
-const api_id = 14127130 ;
+const api_id =  ;
 
 //Paste API_HASH in the quotation
-const api_hash = "508910f224a3cc9f2c8d7d8967560b2d";
+const api_hash = "";
 
 // Your created session paste in quotation
-const session = "1BQANOTEuMTA4LjU2LjE3NQG7ZQaQqICv3iURLcgQEt09poudAHZ/Opm+SrK0hMyaX/rZM/9j349cLPXFSFoxPP3IS/11hx/xcNkNtXbNfDP+pcHY1QKDWQ1l9onu/vLmMoWTF9knFCXX3uLEG3ygfCu30VOYN/cR+AZe5H1MJNR3oKud6+6w5hslJJQ/x0sG9U5s3z5E49hRIayVO+V0QHGmigt9cb2oX/puY+lG4NmTvy85ZgIPL1QakvWUqEuL4ra99zChyhnUv1DORH6FprtKTsRQCZniAXxP2aXRcKxSCl+cVD42VchuBIPYhKoE2AMe+/VmfC+U98qAYR3YqXqmsO7Pr2w604owAcoUVrpzYw==";
+const session = "";
 
 //Paste group username quotation with comma
 const groupID = ["test2345674","raljkasdfkl","hdbdbdhsjsh"];
